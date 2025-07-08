1f777ba631921026e6865735051b420d07bb91f2
Kanishka Misra <menogetusername@gmail.com>
2025-07-07 19:37:47 -0500

    some new annotations

--- transfer.sh
+++ transfer.sh
@@ -2,16 +2,18 @@
 
 ## Run only if you are Kanishka Misra :P 
 
-cp ../induction/data/xcslb_compressed.csv data/
+MEGA_DIR=../induction-nomodels/data
 
-cp ../induction/data/post_annotation_data/feature_metadata.csv data/
+cp $MEGA_DIR/xcslb_compressed.csv data/
 
-cp ../induction/data/concept_senses.csv data/
+cp $MEGA_DIR/post_annotation_data/feature_metadata.csv data/
 
-cp ../induction/data/concept_matrix.txt data/
+cp $MEGA_DIR/concept_senses.csv data/
 
-cp ../induction/data/post_annotation_data/post_annotation_all_full.csv data/xcslb.csv
+cp $MEGA_DIR/concept_matrix.txt data/
 
-python negate.py
+cp $MEGA_DIR/post_annotation_data/post_annotation_all_full.csv data/xcslb.csv
 
-python pluralize.py
+python src/negate_and_pluralize.py
+
+#python pluralize.py
